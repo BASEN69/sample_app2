8caee5784ec8633b69bcea94a10c21cbfe3319fa
TakuroSemba <171462421+BASEN69@users.noreply.github.com>
2024-07-09 07:42:15 +0000

    次回　bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,19 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
+  
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
+  
     @user = User.find(params[:id])
   end
 
   def update
+ 　
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path
@@ -19,5 +24,13 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+  
+  #ユーザー本人のみアクセス可能にする
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_image_path
+    end
   end
 end
